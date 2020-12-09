feb44b915fc51a7c37a111e0d629ce0647d4e7fa
Mia Israel <70295357+miaisrael@users.noreply.github.com>
2020-12-08 19:44:30 -0500

    Update index.html and app.py

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request
 from flask_pymongo import PyMongo
 import scrape_mars
+import os
 
 # Create Flask
 app = Flask(__name__)
@@ -10,19 +11,21 @@ mongo= PyMongo(app, uri="mongodb://localhost:27017/mars_app"
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars = mars)
+
+    mars_info = mongo.db.mars.find_one()
+    return render_template("index.html", mars_info = mars_info)
 
 # Route that will trigger the scrape function    
 @app.route("/scrape")
+
 def scrape():
 
     # Scrape function
-    mars = mongo.db.mars
+    mars_info = mongo.db.mars_info
     data = scrape_mars.scrape()
     
     # Update the Mongo Database using update and upsert=True
-    mars.update({}, data, upsert=True)
+    mars_info.update({}, data, upsert=True)
     
     # Redirect back to home page
     return redirect("/", code=302)
